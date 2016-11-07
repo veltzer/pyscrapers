a079a4bbe5f94dae33a5b34506680fb0610bcbf8
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-07 21:11:53 +0200

    fixes

--- src/scrape/scrape_ig_photos.py
+++ src/scrape/scrape_ig_photos.py
@@ -43,106 +43,110 @@ logging.basicConfig()
 logger=logging.getLogger('scrape')
 logger.setLevel(logging.INFO)
 
-# constants
-domain='www.instagram.com'
-base='https://{domain}'.format(domain=domain)
+def main():
+    # constants
+    domain='www.instagram.com'
+    base='https://{domain}'.format(domain=domain)
 
-# command line parsing
-parser = argparse.ArgumentParser(
-        description='''download photos from instagram'''
-)
-parser.add_argument(
-        '-i',
-        '--id', 
-        help='''id of the user to download the albums of
-        For instance if you see a url like this:
-            https://www.instagram.com/[user_id]
-        then the id for this script will be:
-            [user_id]
-        '''
-)
-parser.add_argument(
-        '-d',
-        '--debug',
-        help='debug requests',
-        default=False,
-        action='store_true',
-)
-parser.add_argument(
-        '-s',
-        '--start',
-        help='start number for image names',
-        type=int,
-        default=0,
-)
-args = parser.parse_args()
-if args.id is None:
-    parser.error('-i/--id must be given')
-if args.debug:
-    logger.setLevel(logging.DEBUG)
-    scrape.utils.debug_requests()
+    # command line parsing
+    parser = argparse.ArgumentParser(
+            description='''download photos from instagram'''
+    )
+    parser.add_argument(
+            '-i',
+            '--id', 
+            help='''id of the user to download the albums of
+            For instance if you see a url like this:
+                https://www.instagram.com/[user_id]
+            then the id for this script will be:
+                [user_id]
+            '''
+    )
+    parser.add_argument(
+            '-d',
+            '--debug',
+            help='debug requests',
+            default=False,
+            action='store_true',
+    )
+    parser.add_argument(
+            '-s',
+            '--start',
+            help='start number for image names',
+            type=int,
+            default=0,
+    )
+    args = parser.parse_args()
+    if args.id is None:
+        parser.error('-i/--id must be given')
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        scrape.utils.debug_requests()
 
-# load cookies from browser
-cookies=browser_cookie3.firefox()
-if args.debug:
-    scrape.utils.print_cookies(cookies, domain)
+    # load cookies from browser
+    cookies=browser_cookie3.firefox()
+    if args.debug:
+        scrape.utils.print_cookies(cookies, domain)
 
-url='{base}/{id}/'.format(base=base, id=args.id)
-logger.debug('url is [%s]', url)
+    url='{base}/{id}/'.format(base=base, id=args.id)
+    logger.debug('url is [%s]', url)
 
-# start a session
-s = requests.Session()
-s.cookies=cookies
+    # start a session
+    s = requests.Session()
+    s.cookies=cookies
 
-r = s.get(url)
-root = scrape.utils.get_real_content(r)
-#scrape.utils.print_element(root)
+    r = s.get(url)
+    root = scrape.utils.get_real_content(r)
+    #scrape.utils.print_element(root)
 
-urls=[]
-e_a = root.xpath('//script[re:match(text(), "^window._sharedData")]')
-assert len(e_a)==1
-e_a = e_a[0]
-data = e_a.text
-json_text = data[data.find('{'):data.rfind('}')+1]
-#print(json_text)
-d=json.loads(json_text)
-l=d['entry_data']['ProfilePage']
-assert(len(l)==1)
-c=l[0]["user"]
-if 'profile_pic_url_hd' in c:
-    urls.append(c['profile_pic_url_hd'])
-elif 'profile_pic_url' in c:
-    urls.append(c['profile_pic_url'])
-user_id=c['id']
-#json.dump(c, sys.stdout, indent=4)
-list_node=c['media']['nodes']
-#for x in list_node:
-#    urls.append(x['display_src'])
+    urls=[]
+    e_a = root.xpath('//script[re:match(text(), "^window._sharedData")]')
+    assert len(e_a)==1
+    e_a = e_a[0]
+    data = e_a.text
+    json_text = data[data.find('{'):data.rfind('}')+1]
+    #print(json_text)
+    d=json.loads(json_text)
+    l=d['entry_data']['ProfilePage']
+    assert(len(l)==1)
+    c=l[0]["user"]
+    if 'profile_pic_url_hd' in c:
+        urls.append(c['profile_pic_url_hd'])
+    elif 'profile_pic_url' in c:
+        urls.append(c['profile_pic_url'])
+    user_id=c['id']
+    #json.dump(c, sys.stdout, indent=4)
+    list_node=c['media']['nodes']
+    #for x in list_node:
+    #    urls.append(x['display_src'])
 
-# now we need to do the follow up query
-url2='{base}/query/'.format(base=base)
-''' g_user(278094193)+{+media.after(732083027810814056,+12)+{++count,++nodes+{++++caption,++++code,++++comments+{++++++count++++},++++comments_disabled,++++date,++++dimensions+{++++++height,++++++width++++},++++display_src,++++id,++++is_video,++++likes+{++++++count++++},++++owner+{++++++id++++},++++thumbnail_src,++++video_views++},++page_info}+}" '''
-data={
-    # the 5000 is the number of images you want (big number to get all)
-    # the 0 in media.after is after what. 0 seems to return everything I think
-    #'q':'ig_user({0})'.format(user_id)+'{ media.after(0, 5000) { count, nodes { display_src } } }',
-    'q':'ig_user({0})'.format(user_id)+'{ media.after(0, 5000) { nodes { display_src } } }',
-    #'q':'ig_user({0})'.format(user_id)+' { media.after(0, 5000) { count, nodes { caption, code, comments { count }, comments_disabled, date, dimensions { height, width }, display_src, id, is_video, likes { count }, owner { id }, thumbnail_src, video_views }, page_info } }',
-    'ref':'users::show',
-    # this is constant as far as I can tell
-    'query_id':'17842962958175392',
-}
-headers={
-    # these two are neccessary or you wont get response from instagram
-    'X-CSRFToken':r.cookies['csrftoken'],
-    'Referer': url,
-}
-# you must send cookies and headers to get the data...
-r2 = s.post(url2, data=data, headers=headers)
-root = scrape.utils.get_real_content(r2)
-res=json.loads(root.text)
-for node in res['media']['nodes']:
-    urls.append(node['display_src'])
-#scrape.utils.print_element(root)
+    # now we need to do the follow up query
+    url2='{base}/query/'.format(base=base)
+    ''' g_user(278094193)+{+media.after(732083027810814056,+12)+{++count,++nodes+{++++caption,++++code,++++comments+{++++++count++++},++++comments_disabled,++++date,++++dimensions+{++++++height,++++++width++++},++++display_src,++++id,++++is_video,++++likes+{++++++count++++},++++owner+{++++++id++++},++++thumbnail_src,++++video_views++},++page_info}+}" '''
+    data={
+        # the 5000 is the number of images you want (big number to get all)
+        # the 0 in media.after is after what. 0 seems to return everything I think
+        #'q':'ig_user({0})'.format(user_id)+'{ media.after(0, 5000) { count, nodes { display_src } } }',
+        'q':'ig_user({0})'.format(user_id)+'{ media.after(0, 5000) { nodes { display_src } } }',
+        #'q':'ig_user({0})'.format(user_id)+' { media.after(0, 5000) { count, nodes { caption, code, comments { count }, comments_disabled, date, dimensions { height, width }, display_src, id, is_video, likes { count }, owner { id }, thumbnail_src, video_views }, page_info } }',
+        'ref':'users::show',
+        # this is constant as far as I can tell
+        'query_id':'17842962958175392',
+    }
+    headers={
+        # these two are neccessary or you wont get response from instagram
+        'X-CSRFToken':r.cookies['csrftoken'],
+        'Referer': url,
+    }
+    # you must send cookies and headers to get the data...
+    r2 = s.post(url2, data=data, headers=headers)
+    root = scrape.utils.get_real_content(r2)
+    res=json.loads(root.text)
+    for node in res['media']['nodes']:
+        urls.append(node['display_src'])
+    #scrape.utils.print_element(root)
 
-scrape.utils.download_urls(urls, start=args.start)
+    scrape.utils.download_urls(urls, start=args.start)
+
+if __name__ == '__main__':
+    main()
--- src/scrape/scrape_tg_photos.py
+++ src/scrape/scrape_tg_photos.py
@@ -31,40 +31,44 @@ logger=logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 #logger.setLevel(logging.DEBUG)
 
-# command line parsing
-parser = argparse.ArgumentParser(
-        description='''download photos from travelgirls'''
-)
-parser.add_argument(
-        '-i',
-        '--id', 
-        help='''id of the user to download the albums of
-        For instance if you see a url like this:
-            http://www.travelgirls.com/member/[user_id]
-        then the id for this script will be:
-            [user_id]
-        '''
-)
-args = parser.parse_args()
-if args.id is None:
-    parser.error('-i/--id must be given')
+def main():
+    # command line parsing
+    parser = argparse.ArgumentParser(
+            description='''download photos from travelgirls'''
+    )
+    parser.add_argument(
+            '-i',
+            '--id', 
+            help='''id of the user to download the albums of
+            For instance if you see a url like this:
+                http://www.travelgirls.com/member/[user_id]
+            then the id for this script will be:
+                [user_id]
+            '''
+    )
+    args = parser.parse_args()
+    if args.id is None:
+        parser.error('-i/--id must be given')
 
-# load cookies from browser
-cookies=browser_cookie3.firefox()
+    # load cookies from browser
+    cookies=browser_cookie3.firefox()
 
-main_url='http://www.travelgirls.com/member/{id}'.format(id=args.id)
-r = requests.get(main_url, cookies=cookies)
-root = scrape.utils.get_real_content(r)
+    main_url='http://www.travelgirls.com/member/{id}'.format(id=args.id)
+    r = requests.get(main_url, cookies=cookies)
+    root = scrape.utils.get_real_content(r)
 
-urls=[]
-e_a = root.xpath('//a[contains(@class,\'photo\')]')
-for x in e_a:
-    #print(lxml.etree.tostring(x, pretty_print=True))
-    children=x.getchildren()
-    assert len(children)==1
-    img=children[0]
-    url=scrape.utils.add_http(img.attrib['src'], main_url)
-    url=url.replace('mini','')
-    urls.append(url)
+    urls=[]
+    e_a = root.xpath('//a[contains(@class,\'photo\')]')
+    for x in e_a:
+        #print(lxml.etree.tostring(x, pretty_print=True))
+        children=x.getchildren()
+        assert len(children)==1
+        img=children[0]
+        url=scrape.utils.add_http(img.attrib['src'], main_url)
+        url=url.replace('mini','')
+        urls.append(url)
 
-scrape.utils.download_urls(urls)
+    scrape.utils.download_urls(urls)
+
+if __name__ == '__main__':
+    main()
