fa047c8d7cde7fc5bdd6cb635dfbd0adb117c91b
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-19 11:22:15 +0200

    fixed from pycharm

--- src/scrape/scrape_mb_photos.py
+++ src/scrape/scrape_mb_photos.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python3
 
-'''
-This script scrapes photos from mamba.ru.
-
-For instance if you see a url like this:
-    http://www.mamba.ru/mb[number]
-then the id for this script will be:
-    mb[number]
-'''
-
-import requests # for post
-import lxml.html # for fromstring
-import lxml.etree # for tostring
-import json # for loads
-import shutil # for copyfileobj
-import sys # for argv
-import logging # for basicConfig, getLogger
+import requests  # for post
+import logging  # for basicConfig, getLogger
 import argparse  # for ArgumentParser
-import browser_cookie3 # for firefox
-import scrape.utils # for download_urls, get_real_content
+import browser_cookie3  # for firefox
+import scrape.utils  # for download_urls, get_real_content
 
 
 # set up the logger
 logging.basicConfig()
-logger=logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-#logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.DEBUG)
+
 
 def main():
     # command line parsing
     parser = argparse.ArgumentParser(
-            description='''download photos from travelgirls'''
+            description='''download photos from mamba.ru'''
     )
     parser.add_argument(
             '-i',
             '--id', 
             help='''id of the user to download the albums of
             For instance if you see a url like this:
-                http://www.travelgirls.com/member/[user_id]
+                http://www.mamba.ru/mb[number]
             then the id for this script will be:
-                [user_id]
+                mb[number]
             '''
     )
     args = parser.parse_args()
@@ -47,24 +34,12 @@ def main():
         parser.error('-i/--id must be given')
 
     # load cookies from browser
-    cookies=browser_cookie3.firefox()
+    cookies = browser_cookie3.firefox()
 
-    main_url='http://www.travelgirls.com/member/{id}'.format(id=args.id)
+    main_url = 'http://www.mamba.ru/{id}'.format(id=args.id)
     r = requests.get(main_url, cookies=cookies)
     root = scrape.utils.get_real_content(r)
-
-    urls=[]
-    e_a = root.xpath('//a[contains(@class,\'photo\')]')
-    for x in e_a:
-        #print(lxml.etree.tostring(x, pretty_print=True))
-        children=x.getchildren()
-        assert len(children)==1
-        img=children[0]
-        url=scrape.utils.add_http(img.attrib['src'], main_url)
-        url=url.replace('mini','')
-        urls.append(url)
-
-    scrape.utils.download_urls(urls)
+    print(root)
 
 if __name__ == '__main__':
     main()
