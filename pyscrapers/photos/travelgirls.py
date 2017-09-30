cc10f938f4e0014c2350203fb1d0cdde1928449d
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-30 18:05:15 +0300

    move to https when I can

--- pyscrapers/photos/travelgirls.py
+++ pyscrapers/photos/travelgirls.py
@@ -6,7 +6,7 @@ import pyscrapers.core.utils
 
 
 def scrape_travelgirls(user_id: str, cookies) -> List[str]:
-    main_url = 'http://www.travelgirls.com/member/{user_id}'.format(user_id=user_id)
+    main_url = 'https://www.travelgirls.com/member/{user_id}'.format(user_id=user_id)
     r = requests.get(main_url, cookies=cookies)
     root = pyscrapers.core.utils.get_real_content(r)
 
