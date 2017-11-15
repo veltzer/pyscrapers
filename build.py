d330d169f2a7009dd880e82d09dbcff0a5a019df
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-15 18:07:40 +0200

    added docs

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from pydmt.builders.sphinx import Sphinx
 from pydmt.core.pydmt import PyDMT
 from pydmt.features.templating import Templating
 import pylogconf
@@ -8,4 +9,6 @@ pylogconf.setup()
 pydmt = PyDMT()
 f = Templating()
 f.setup(pydmt)
+b = Sphinx(package_name="pyscrapers")
+pydmt.add_builder(b)
 pydmt.build_all()
