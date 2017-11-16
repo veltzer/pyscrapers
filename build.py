a79be17006ab56680a8881c47cfe47478e924745
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-16 20:02:37 +0200

    new mako framework

--- build.py
+++ build.py
@@ -4,12 +4,13 @@ from pydmt.builders.sphinx import Sphinx
 from pydmt.core.pydmt import PyDMT
 from pydmt.features.templating import Templating
 import pylogconf
+import definitions.project
 
 pylogconf.setup()
 pydmt = PyDMT()
 f = Templating()
 f.setup(pydmt)
-b = Sphinx(package_name="pyscrapers")
+b = Sphinx(package_name=definitions.project.project_name)
 pydmt.add_builder(b)
 stats = pydmt.build_all()
 # print(stats.builder_fail[0][1])
