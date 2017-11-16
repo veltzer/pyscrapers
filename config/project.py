3dd495060888961874ac75de1994e7d480c5ca39
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-16 20:02:37 +0200

    new mako framework

---
+++ config/project.py
@@ -0,0 +1,43 @@
+"""
+project definitions
+"""
+
+project_github_username = 'veltzer'
+project_name = 'pyscrapers'
+# project_website = 'https://{project_github_username}.github.io/{project_name}'.format(**locals())
+project_website = 'https://github.com/{project_github_username}/{project_name}'.format(**locals())
+project_website_source = 'https://github.com/{project_github_username}/{project_name}'.format(**locals())
+project_website_git = 'git://github.com/{project_github_username}/{project_name}.git'.format(**locals())
+project_website_download = 'https://launchpanet/~mark-veltzer/+archive/ubuntu/ppa'
+# project_paypal_donate_button_id='ASPRXR59H2NTQ'
+# project_google_analytics_tracking_id='UA-56436979-1'
+project_long_description = 'project to produce various useful scrapers'
+project_short_description = 'project to produce various useful scrapers'
+# keywords to put on html pages or for search, dont put the name of the project or my details
+# as they will be added automatically...
+project_keywords = [
+    'scrape',
+    'images',
+    'social',
+    'facebook',
+    'instagram',
+    'vk.com',
+    'download',
+    'pics',
+]
+project_license = 'MIT'
+project_year_started = '2016'
+project_description = 'A collection of scrapers for the web'
+project_platforms = [
+    'python3',
+]
+project_classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Utilities',
+]
+project_data_files = []
+# project_data_files.append(templar.utils.hlp_files_under('/usr/bin', 'src/*'))
