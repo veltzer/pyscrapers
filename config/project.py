8ff9a9a879c76ee0c03d2d5c66f2a701d7dcccfb
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-03 10:55:06 +0200

    better config

--- config/project.py
+++ config/project.py
@@ -6,8 +6,7 @@ import pyclassifiers.values
 
 project_github_username = 'veltzer'
 project_name = 'pyscrapers'
-# project_website = 'https://{project_github_username}.github.io/{project_name}'.format(**locals())
-project_website = 'https://github.com/{project_github_username}/{project_name}'.format(**locals())
+project_website = 'https://{project_github_username}.github.io/{project_name}'.format(**locals())
 project_website_source = 'https://github.com/{project_github_username}/{project_name}'.format(**locals())
 project_website_git = 'git://github.com/{project_github_username}/{project_name}.git'.format(**locals())
 project_website_download_ppa = 'https://launchpanet/~mark-veltzer/+archive/ubuntu/ppa'
