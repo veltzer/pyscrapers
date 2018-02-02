92fdb86894cb37f14bd5359c4d3d8afc8f3461da
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-02 21:59:16 +0200

    bump version

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
-version = (0, 0, 6)
+version = (0, 0, 7)
 version_str = ".".join(str(x) for x in version)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pyscrapers',
-    version='0.0.6',
+    version='0.0.7',
     packages=[
         'pyscrapers',
         'pyscrapers.core',
