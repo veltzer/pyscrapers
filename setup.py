d479ea530fc7a8176b6862b433726fe07961c1cd
Mark Veltzer <mark.veltzer@gmail.com>
2019-06-20 12:16:14 +0300

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pyscrapers',
-    version='0.0.12',
+    version='0.0.13',
     packages=[
         'pyscrapers',
         'pyscrapers.core',
