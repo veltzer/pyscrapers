bee5694cd0b5fef5e490e345743aa99d09c1e7fd
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-24 17:49:59 +0300

    new version

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
-version = (0, 0, 20)
+version = (0, 0, 21)
 version_str = ".".join(str(x) for x in version)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pyscrapers',
-    version='0.0.20',
+    version='0.0.21',
     packages=[
         'pyscrapers',
         'pyscrapers.core',
