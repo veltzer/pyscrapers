e7f8af4171b5ddd229ac323cd23eb9161f40c4bc
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-07 17:04:55 +0300

    classifiers

--- config/project.py
+++ config/project.py
@@ -39,6 +39,7 @@ project_classifiers = [
     pyclassifiers.values.Environment__Console,
     pyclassifiers.values.OperatingSystem__OSIndependent,
     pyclassifiers.values.ProgrammingLanguage__Python,
+    pyclassifiers.values.ProgrammingLanguage__Python__3,
     pyclassifiers.values.ProgrammingLanguage__Python__3__Only,
     pyclassifiers.values.Topic__Utilities,
     pyclassifiers.values.License__OSIApproved__MITLicense,
