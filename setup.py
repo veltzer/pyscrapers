cf03763d21ee6992ace03c4950d26c880cbe079d
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-17 08:05:29 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pyscrapers",
-    version="0.0.36",
+    version="0.0.37",
     packages=[
         'pyscrapers',
         'pyscrapers.core',
