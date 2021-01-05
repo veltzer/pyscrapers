3be9cf7390c87e243d2d5f98eb2eeb869840dc02
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-05 18:00:55 +0200

    new version

--- pyscrapers/static.py
+++ pyscrapers/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.44"
+VERSION_STR = "0.0.45"
 DESCRIPTION = "project to produce various useful scrapers"
 APP_NAME = "pyscrapers"
 LOGGER_NAME = "pyscrapers"
