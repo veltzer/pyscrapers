b08f05c176352b74c44c638a92b7507d1f620275
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-16 09:09:57 +0200

    new version

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
-version = (0, 0, 50)
+version = (0, 0, 51)
 version_str = ".".join(str(x) for x in version)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pyscrapers",
-    version="0.0.50",
+    version="0.0.51",
     packages=[
         'pyscrapers',
         'pyscrapers.core',
