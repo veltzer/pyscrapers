62c5bb2093b82ae693be61db75990723f91cb9e2
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-12 18:39:06 +0200

    added long file name protection

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pyscrapers",
-    version="0.0.52",
+    version="0.0.53",
     packages=[
         'pyscrapers',
         'pyscrapers.core',
