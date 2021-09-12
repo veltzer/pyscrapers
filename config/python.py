b099099e9127f6d3d6d04aa32b0b7aafaeeb66c1
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-12 21:01:57 +0300

    python version bump

--- config/python.py
+++ config/python.py
@@ -45,5 +45,5 @@ extras_require = {
 }
 
 test_os = "[ubuntu-18.04, ubuntu-20.04]"
-test_python = "[3.7, 3.8]"
+test_python = "[3.7, 3.8, 3.9]"
 test_container = "[ 'ubuntu:18.04', 'ubuntu:20.04' ]"
