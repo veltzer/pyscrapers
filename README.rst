fabf130583a0ed3f392f4fcdf922102f2973a4d6
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-28 02:57:50 +0300

    new version

--- README.rst
+++ README.rst
@@ -12,7 +12,7 @@ project website: https://veltzer.github.io/pyscrapers
 
 author: Mark Veltzer
 
-version: 0.0.58
+version: 0.0.59
 
 What is it?
 -----------
--- pyscrapers/static.py
+++ pyscrapers/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.58"
+VERSION_STR = "0.0.59"
 DESCRIPTION = "project to produce various useful scrapers"
 APP_NAME = "pyscrapers"
 LOGGER_NAME = "pyscrapers"
