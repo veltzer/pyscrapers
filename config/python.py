758ad0e3c683b29a4c601352be8558e9363b88bd
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-01 07:49:20 +0300

    unification

--- config/python.py
+++ config/python.py
@@ -43,6 +43,6 @@ extras_require = {
 }
 
 python_requires = ">=3.9"
-test_os = [ "ubuntu-20.04" ]
-test_python = [ "3.9" ]
-test_container = [ "ubuntu:20.04" ]
+test_os = ["ubuntu-20.04"]
+test_python = ["3.9"]
+test_container = ["ubuntu:20.04"]
