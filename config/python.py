6d7c1cf03e46ed609f3ab10f31309ffd2c7cdb8f
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:20 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,47 +3,38 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pyscrapers=pyscrapers.main:main',
+    "pyscrapers=pyscrapers.main:main",
 ]
 
 run_requires = [
-]
-
-setup_requires = [
-]
-
-install_requires = [
-    'lxml',
-    'requests',
-    # 'hyper',
-    'browser-cookie3',
-    'pytconf',
-    'pylogconf',
-    'pornhub-api',
-    'youtube-dl',
-    'pyeventroute',
+    "lxml",
+    "requests",
+    # "hyper",
+    "browser-cookie3",
+    "pytconf",
+    "pylogconf",
+    "pornhub-api",
+    "youtube-dl",
+    "pyeventroute",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'pyflakes',
-    'pyre-check',
-    'flake8',
-    'pymakehelper',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "pyflakes",
+    "pyre-check",
+    "flake8",
+    "pymakehelper",
 ]
 
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
 ]
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
