f4f527efbba577401c28382c23db4625560fe02f
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-12 13:24:15 +0300

    no commit message given

--- pyscrapers/workers/pornhub.py
+++ pyscrapers/workers/pornhub.py
@@ -68,6 +68,7 @@ def download_search() -> None:
         if ConfigPornhubSearch.use_tags:
             kwargs["tags"] = ConfigPornhubSearch.tags
         try:
+            # pylint: disable=no-member
             data = api.search.search_videos(
                 ConfigPornhubSearch.query,
                 page=page,
