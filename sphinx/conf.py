99520969971fd10b1a80c9c8e78209dabc67ed54
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-16 09:40:24 +0200

    better sphinx configuration

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -18,6 +18,11 @@ release = ".".join(str(x) for x in config.version.tup)
 html_theme_options = {
         "show_powered_by": False,
 }
+# allow us to use |project| in our snipplets and rst files
 rst_epilog = f"""
 .. |project| replace:: {project}
 """
+# title without a version
+html_title = '%s Documentation' % project
+# This is the default
+# html_title = '%s %s Documentation' % (project, version)
