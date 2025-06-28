529d52cd0eb87a682b1ae49da063665d32ff12cc
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 01:24:22 +0300

    no commit message given

--- src/pyscrapers/workers/youtube_dl_handlers.py
+++ src/pyscrapers/workers/youtube_dl_handlers.py
@@ -6,7 +6,6 @@ References:
 - https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312
 """
 import logging
-from typing import List
 
 import youtube_dl
 
