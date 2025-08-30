344532efb3c5092138c2c826d1552a22c626dfaa
Mark Veltzer <mark.veltzer@gmail.com>
2025-08-31 01:44:31 +0300

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -5,8 +5,8 @@ os level dependencies for this project
 packages = [
     # this is for the compilation of the "dbus-python" python package which is
     # half native
-    "libdbus-glib-1-dev",
-    "libdbus-1-dev",
+    # "libdbus-glib-1-dev",
+    # "libdbus-1-dev",
     # glib
-    "libglib2.0-dev",
+    # "libglib2.0-dev",
 ]
